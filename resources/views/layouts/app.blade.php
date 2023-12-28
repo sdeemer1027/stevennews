83f1d1c469ab6ccb07b368e8ad46bea91144bbe8
Steve Deemer <steve@capeandbay.com>
2023-12-28 12:41:22 -0500

    color - menu - icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,15 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+
+
+
+
+
+
+
+
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
@@ -17,13 +26,17 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+<style>
+
+</style>
+
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+<body  data-bs-theme="dark">
+    <div id="app" >
+        <nav class="navbar navbar-expand-md navbar-light bg-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/') }}"> <img src="srdlogo.png" height="50px;">
+                  <span style="color:#e97855;">S</span>teven.<span style="color:#e97855;">News</span>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -37,6 +50,28 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>{{ __('About') }}</a>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">About Steve</a>
+                                    <a class="dropdown-item" href="#">About Site</a>
+                                    <a class="dropdown-item" href="#">About Evets.Pet</a>
+                                </div>
+                            </li>
+                        <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>{{ __('Services') }}</a>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Database</a>
+                                    <a class="dropdown-item" href="#">PHP</a>
+                                    <a class="dropdown-item" href="#">Laravel</a>
+                                </div>
+                            </li>
+
+
+
+
+                        <a class="nav-link" href="#">{{ __('Contact') }}</a>
+                        
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
@@ -62,6 +97,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+
+<a class="dropdown-item" href="{{ route('dashboard.index') }}">Dashboard</a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
