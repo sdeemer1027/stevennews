7e5d59060f3d576cc52a1d7cf66efd54247ed6ef
Steve Deemer <steve@capeandbay.com>
2024-04-03 18:45:44 -0400

    sitemap generate

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -4,6 +4,9 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Sitemap;
+use App\Models\Article;
+
 
 class GenerateSitemap extends Command
 {
@@ -13,9 +16,56 @@ class GenerateSitemap extends Command
 
     public function handle()
     {
+       
+   $sitemap = Sitemap::create();
+
+        // Add your homepage URL first
+        $sitemap->add(route('welcome'));
+
+        // Fetch dynamic URLs from database and add them to the sitemap
+        $articles = Article::all(); // Example: Fetch all posts from 'posts' table
+        foreach ($articles as $article) {
+
+//dd($article);
+
+
+            $sitemap->add(route('articles.show', $article->id)); // Assuming you have a route for individual posts
+        }
+
+        // Write sitemap to file
+        $sitemap->writeToFile(public_path('sitemap.xml'));
+
+        $this->info('Sitemap generated successfully.');
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
         SitemapGenerator::create(config('app.url'))
-            ->writeToFile(public_path('sitemap.xml'));
+         ->add('/'); // Add your homepage first
+
+         // Fetch dynamic URLs from database and add them to the sitemap
+        $articles = Article::all(); // Example: Fetch all posts from 'posts' table
+        foreach ($articles as $article) {
+            SitemapGenerator::add(route('articles.show', $articles->slug)); // Assuming you have a route for individual posts
+        }
+
+        SitemapGenerator::writeToFile(public_path('sitemap.xml'));
+        
+  //          ->writeToFile(public_path('sitemap.xml'));
         
         $this->info('Sitemap generated successfully.');
+
+        */
     }
 }
