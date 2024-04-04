23a07719fc5b7d8078864c086bdca395bf573a3a
Steve Deemer <steve@capeandbay.com>
2024-04-04 16:59:31 -0400

    more

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -28,6 +28,7 @@
         @foreach($users as $user)
             <tr>
                 <td>{{ $user->name }}</td>
+                  <td>{{ $user->email }}</td>
                 @foreach($countroles as $countrole)
                     <td>
                         @php
