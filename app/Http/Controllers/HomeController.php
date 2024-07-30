302632ddb117191b526f314dd6ac58f4e9837641
Steve Deemer <it@xcsat.com>
2024-07-30 09:25:18 -0400

    day

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
     public function index()
     {
 
-$abc = ["data" => "some friggen content"];
+$abc = ["data" => "some content"];
 $cntusers= User::count();
 $usersWithRoles = User::with('roles')->get();
 
